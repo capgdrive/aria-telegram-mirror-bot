1bcf0655ba25ef0c66ce2042eb050e3d8a3686c6
capgdrive <59224696+capgdrive@users.noreply.github.com>
2019-12-26 13:37:48 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -7,7 +7,7 @@ module.exports = Object.freeze({
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
   GDRIVE_PARENT_DIR_ID: '13d-SS4kev97DWlGX9WPr9eqHiv6X39At',
-  SUDO_USERS: [],	// Telegram user IDs. These users can use the bot in any chat.
+  SUDO_USERS: [825558831,  839423892],	// Telegram user IDs. These users can use the bot in any chat.
   AUTHORIZED_CHATS: [1001441510922],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
